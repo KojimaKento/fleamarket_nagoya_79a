2735a5fbb4b721e45f1d782b05b872496bdd80cd
kogayama <kogayama0130@gmail.com>
2020-11-01 16:01:52 +0900

    edited items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def article_params
+    params.require(:item).permit(:name,:introduction, :price, :category_id, :brand, :condition_id, :shipping_date_id, :delivery_source_area_id)
+  end
+
 end

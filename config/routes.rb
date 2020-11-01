f6e83e1d53540b6a23b8f9bc8d934e41dc7e60f7
kotoya87 <lingmuqinye@gmail.com>
2020-11-01 14:01:37 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#_exhibition'
+  root 'items#exhibitions'
   devise_for :users
   get 'mypages', to: 'mypages#show'
+  get 'exhibitions', to: 'exhibitions#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

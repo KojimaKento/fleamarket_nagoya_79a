fe66a59f8827b93ec24d6c7f3dadcf0937a0489b
kogayama <kogayama0130@gmail.com>
2020-11-08 20:17:45 +0900

    added profile table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_005516) do
+ActiveRecord::Schema.define(version: 2020_11_08_110646) do
 
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "src", null: false
@@ -36,6 +36,20 @@ ActiveRecord::Schema.define(version: 2020_11_03_005516) do
     t.integer "buyer_id"
   end
 
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "birthday", null: false
+    t.string "introduction"
+    t.string "avatar"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -50,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_11_03_005516) do
   end
 
   add_foreign_key "item_images", "items"
+  add_foreign_key "profiles", "users"
 end

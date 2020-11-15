746a93d225fefc7daa7f069b8669313bb82f595c
KojiK222 <68413995+KojiK222@users.noreply.github.com>
2020-11-15 17:23:43 +0900

    logout_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
+  def logout
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   
   root 'items#index'
   devise_for :users
+  
 
   resources :items, only: [:new, :create]
-  resources :users, only: [:show, :new]
+  resources :users, only: [:show]
+  get "users/:id/logout", to: "users#logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

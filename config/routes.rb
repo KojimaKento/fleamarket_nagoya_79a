c06ff8bf705c5ad632b0797bbdf5a188eecec3e4
KojiK222 <68413995+KojiK222@users.noreply.github.com>
2020-11-17 09:39:42 +0900

    credit_card_logout_front

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :items, only: [:new, :create]
   resources :users, only: [:show]
+  get "users/:id/credit", to: "users#credit"
   get "users/:id/logout", to: "users#logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
